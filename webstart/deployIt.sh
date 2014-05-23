ee5b25287ac9191f0076f62b144846ce91237c05
Dave Green <greendavegreen@gmail.com>
2014-05-23 12:28:32 -0400

    deploy script for pushing jars up to staging server

---
+++ webstart/deployIt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ../out/artifacts/ch_jar/
+ssh -i ~/Documents/aws/rhplaykey.pem ec2-user@54.235.254.242 rm -rf /var/local/templates/journals/ripe/webapp/ingest/*
+scp -i ~/Documents/aws/rhplaykey.pem * ec2-user@54.235.254.242:/var/local/templates/journals/ripe/webapp/ingest
